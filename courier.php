457089d46c5aa07e06ff2ddb3447cc46d97101cd
yumi <yumi@howler.space>
2017-12-09 19:41:44 -0500

    Initial fetching of posts within a topic through courier

--- courier.php
+++ courier.php
@@ -2,31 +2,31 @@
 include 'includes/config.php';
 include 'includes/courier.php';
 
+// Initial fetch for reading
 if (isset($_GET['fetch'])) {
 	$timeline = $_GET['timeline'];
 	$topic = $_GET['topic'];
 
 	if (isset($timeline, $topic)) {
-		print json_encode([[
-			'id' => 0,
-			'modified' => '2017-12-07 01:13:24',
-			'content' => "Hello from $timeline topic $topic 【 =◈︿◈= 】"
-		]]);
+		$posts = fetch_posts($timeline, $topic);
+		print json_encode($posts);
 	}
 	else if (isset($timeline)) {
 		$topics = fetch_topics($timeline);
 		print json_encode($topics);
 	}
 }
+// Real-time updates
 if (isset($_GET['subscribe'])) {
 	$timeline = $_GET['timeline'];
 	$topic = $_GET['topic'];
 	$init = isset($_GET['init']);
 
 	if (isset($timeline, $topic)) {
+		$i = 2;
 		while (!sleep(1)) {
 		print json_encode([
-			'id' => 0,
+			'id' => $i++,
 			'modified' => '2017-12-07 01:13:24',
 			'content' => "Hello from $timeline topic #$topic【 =◈︿◈= 】"
 		]);
@@ -34,12 +34,13 @@ if (isset($_GET['subscribe'])) {
 		}
 	}
 	else if (isset($timeline)) {
-		while (!sleep(1)) {
-/*		print json_encode([
-			'id' => 0,
+		$i = 2;
+		while (!sleep(5)) {
+		print json_encode([
+			'id' => $i++,
 			'modified' => '2017-12-07 01:13:24',
 			'content' => "Hello from $timeline 【 =◈︿◈= 】"
-		]);*/
+		]);
 			flush();
 		}
 	}
--- includes/courier.php
+++ includes/courier.php
@@ -20,4 +20,26 @@ function fetch_topics($timeline)
 	}
 	return $ret;
 }
+function fetch_posts($timeline, $topic)
+{
+	$dbh = mysqli_connect(CONFIG_RAL_SERVER,
+		CONFIG_RAL_USERNAME,
+		CONFIG_RAL_PASSWORD,
+		CONFIG_RAL_DATABASE);
+	mysqli_set_charset($dbh, 'utf8');
+	$timeline = mysqli_real_escape_string($dbh, $timeline);
+	$topic = mysqli_real_escape_string($dbh, $topic);
+	$query = "SELECT `Id`, `Owner`, `Content`, Modified AS `Modified` FROM `Posts`"
+	. " WHERE `Timeline`='$timeline' AND `Topic`=$topic AND (Id!=$topic)";
+	$res = mysqli_query($dbh, $query);
+	$ret = [];
+	while ($row = mysqli_fetch_assoc($res)) {
+		$ret[] = [
+			'id' => $row['Id'],
+			'modified' => $row['Modified'],
+			'content' => $row['Content']
+		];
+	}
+	return $ret;
+}
 ?>
