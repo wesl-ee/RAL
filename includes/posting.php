1b1637198f6fb3c9574f8b23cac9c54c5bb6c365
yumi <yumi@howler.space>
2017-12-18 22:00:47 -0500

    Multi-line BBCode is delightfully painful to parse

--- includes/posting.php
+++ includes/posting.php
@@ -2,39 +2,103 @@
 // Magical BBCode parse
 function bbbbbbb($string)
 {
-	$tags = 'b|i|color|quote|url|code';
-	while (preg_match_all('`\[('.$tags.')=?(.*?)\](.+?)\[/\1\]`',
-	$string, $matches))
-	foreach ($matches[0] as $key => $match) {
-		$tag = $matches[1][$key];
-		$param = $matches[2][$key];
-		$innertext = $matches[3][$key];
-		switch ($tag) {
-		case 'b':
-			$replacement = "<strong>$innertext</strong>";
-			break;
-                case 'i':
-			$replacement = "<em>$innertext</em>";
-			break;
-		case 'color':
-			$replacement = "<span style=\"color:"
-			." $param;\">$innertext</span>";
-			break;
-		case 'quote':
-			$replacement = "<blockquote>$innertext</blockquote>"
-			. $param? "<cite>$param</cite>" : '';
-			break;
-		case 'url':
-			$replacement = '<a href="'
-			. ($param ? $param : $innertext) . "\">$innertext</a>";
-			break;
-		case 'code':
-			$replacement = "<pre>$innertext</pre>";
-			break;
+	$lines = explode(PHP_EOL, $string);
+	$newlines = $lines;
+	$opened = [
+		'b' => 0,
+		'i' => 0,
+		'color' => 0,
+		'quote' => 0,
+		'url' => 0,
+		'code' => 0
+	];
+	$commits = [];
+	// First, find all changes which should be made and make note of them
+	for ($i = 0; $i < count($lines); $i++) {
+		$a = 0;
+		while (($a = indexOf($lines[$i], "[", $a) + 1) > 0
+		&& ($b = indexOf($lines[$i], "]", $a)) > $a) {
+			$inside = substr($lines[$i], $a, $b - $a);
+			if ($inside{0} == "/") {
+				$tag = substr($inside, 1);
+				if (!$opened[$tag]) continue;
+
+				switch($tag) {
+				case  'b':
+					$replacement = "</strong>";
+					break;
+				case 'i':
+					$replacement = "</em>";
+					break;
+				case 'color':
+					$replacement = "</span>";
+					break;
+				case 'quote':
+					$replacement = "</blockquote>";
+					break;
+				case 'url':
+					$replacement = "</a>";
+					break;
+				case 'code':
+					$replacement = "</pre>";
+					break;
+				default: continue;
+				}
+
+				// Replace the bbCode tag
+				$lines[$i] = substr($lines[$i], 0, $a - 1)
+				. substr($lines[$i], $b + 1);
+
+				$commits[$i][$a-1] = $replacement;
+				$opened[$tag]--;
+			} else {
+				if (($c = indexOf($inside, "=")) >= 0) {
+					$tag = substr($inside, 0, $c);
+					$param = substr($inside, $c + 1);
+				} else $tag = $inside;
+
+				switch ($tag) {
+				case  'b':
+					$replacement = "<strong>";
+					break;
+				case 'i':
+					$replacement = "<em>";
+					break;
+				case 'color':
+					$replacement = "<span style=color:$param>";
+					break;
+				case 'quote':
+					$replacement = "<blockquote>";
+					break;
+				case 'url':
+					$replacement = "<a href='$param'>";
+					break;
+				case 'code':
+					$replacement = "<pre>";
+					break;
+				default: continue;
+				}
+
+				// Replace the bbCode tag
+				$lines[$i] = substr($lines[$i], 0, $a - 1)
+				. substr($lines[$i], $b + 1);
+
+				$commits[$i][$a-1] = $replacement;
+				$opened[$tag]++;
+			}
 		}
-		$string = str_replace($match, $replacement, $string);
 	}
-	return $string;
+
+	// Commit the changes which were suggested
+	foreach ($commits as $n => $changes) {
+	$o = 0;
+	foreach ($changes as $i => $change) {
+		$lines[$n] = substr($lines[$n], 0, $i + $o)
+		. $change
+		. substr($lines[$n], $i + $o);
+		$o += strlen($change);
+	} }
+	return join(PHP_EOL, $lines);
 }
 // Creates the post on a given (timeline, topic)
 function create_post($timeline, $topic, $auth, $content)
@@ -116,5 +180,33 @@ function create_topic($timeline, $auth, $content)
 	mysqli_query("COMMIT");
 	ralog("Created topic");
 	return true;
+}
+/*
+ * Like strpos but does not loop over the
+ * entire string when given an offset
+*/
+function indexOf($string, $substring, $offset = 0)
+{
+	$stringlen = strlen($string);
+	$sublen = strlen($substring);
+	for ($i = $offset, $j = 0; $i < $stringlen; $i++) {
+		if ($string{$i} == $substring{$j}) {
+			if (!(++$j - $sublen))
+			return $i - $sublen + 1;
+		} else $j = 0;
+	}
+	return -1;
+}
+function lastIndexOf($string, $substring, $offset = 0)
+{
+	$stringlen = strlen($string);
+	$sublen = strlen($substring);
+	for ($i = $stringlen - 1, $j = $sublen; $i + 1 - $offset; $i--) {
+		if ($string{$i} == $substring{$j - 1}) {
+			if (!--$j) return $i;
+		}
+		else $j = $sublen;
+	}
+	return $i - $offset;
 }
 ?>
